699210601343fece7982da0b49927e049cf5cfa2
klikli19 <103356373+klikli19@users.noreply.github.com>
2022-08-26 20:46:47 +0300

    сделала минимальный уровень.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         Book book1 = new Book("Инферно. ", new Author("Дэн", "Браун. "), 2013);
         Book book2 = new Book("Сияние. ", new Author("Стивен", "Кинг. "), 2017);
         Book book3 = new Book("Хроники Сиалы. ", new Author("Алексей", "Пехов. "), 2005);
-        book1.setYearPublishing(2009);
+        book3.setYearPublishing(2009);
         System.out.println("Название книги: " + book3.getName() + "Автор: " + book3.getAuthor().getFirstName() + " " + book3.getAuthor().getLastName() + "Год издания: " + book3.getYearPublishing());
     }
 }
